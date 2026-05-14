557a17a9a77fd27fe8b30f7206afdf9fdf16fbd9
lystar521 <Lystar521@outlook.com>
2026-05-14 16:58:45 +0800

    Add files via upload

---
+++ RTE/_daocheleida_yuyinmokuiai/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'daocheleida_yuyinmokuiai' 
+ * Target:  'daocheleida_yuyinmokuiai' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
